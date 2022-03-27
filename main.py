84dec103e0a3e1ab1e0ed17f8943cfe95f62d2ec
HizRadit07 <hizkiagenius@gmail.com>
2022-03-28 02:16:44 +0700

    fixed carrying -1 messing up the whole shabang

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ input_layer = Layer(2, None, False, "input layer")
 
 #create output layer
 output_layer = Layer(1, "sigmoid", False, "output layer", 0)
-output_layer.getNeuronAtIndex(0).set_weight([-30.0, 20.0, 20.0])
+output_layer.getNeuronAtIndex(0).set_weight([0, 0, 0])
 
 #create hidden layer 1
 hidden_layer1 = Layer(2, "sigmoid", True, "hidden layer 1")
@@ -26,18 +26,22 @@ nn.add_layer(input_layer)
 nn.add_layer(hidden_layer1)
 nn.add_layer(output_layer)
 
-res = nn.predict([0.0, 0.0])
+res = nn.predict([1.0, 0.0])
 print(res)
-nn.printAllNeuronInput()
+# nn.printAllNeuronInput()
+#nn.printAllNeuronWeight()
+#nn.printAllNeuronOutput()
 
-for i in range (10):
-    grad = nn.find_unit_gradient([0.0])
+for i in range (1000):
+    delta = nn.backprop([1.0])
 
-    nn.update_weights(grad, 0.01)
-    res2 = nn.predict([0.0, 0.0])
+    nn.update_weights(delta, 0.7)
+    res2 = nn.predict([1.0, 0.0])
 # for layer in nn.layers:
 #     for neuron in layer.neurons:
 #         print(neuron.weight)
-nn.printAllNeuronInput()
+# nn.printAllNeuronInput()
+#nn.printAllNeuronWeight()
+#nn.printAllNeuronOutput()
 
 print(res2)
--- neuralnetwork.py
+++ neuralnetwork.py
@@ -39,7 +39,7 @@ class NeuralNetwork:
 
     #find unit gradient of neural network
     #output is an array that corresponds to all weight in the neural network
-    def find_unit_gradient(self, target):
+    def backprop(self, target):
         grad = []
         backPropErrTerm = [] #init awal
         nextlayer = None
@@ -51,8 +51,8 @@ class NeuralNetwork:
                 i = 0
                 for neuron in layer.neurons:
                     for x in neuron.x:
-                        grad.append(-1 * (target[i]-neuron.output) * neuron.count_derivative(neuron.output) * x)
-                        backPropErrTerm.append(-1 * (target[i]-neuron.output) * neuron.count_derivative(neuron.output))
+                        grad.append((target[i]-neuron.output) * neuron.count_derivative(neuron.output) * x)
+                        backPropErrTerm.append((target[i]-neuron.output) * neuron.count_derivative(neuron.output))
                     i+=1
                 finalErrTermArr.extend(backPropErrTerm)
             else: #hidden or input layer
@@ -64,8 +64,8 @@ class NeuralNetwork:
                     i = 0
                     for x in neuron.x:
                         nextLayerWeights = nextlayer.get_all_neuron_weight_at_index(i)
-                        grad.append(-1 * neuron.count_derivative(neuron.output) * sumIntAndArray(backPropErrTerm[i], nextLayerWeights) * x)
-                        tempBackPropErrArr.append(-1 * neuron.count_derivative(neuron.output) * sumIntAndArray(backPropErrTerm[i], nextLayerWeights))
+                        grad.append(neuron.count_derivative(neuron.output) * sumIntAndArray(backPropErrTerm[i], nextLayerWeights) * x)
+                        tempBackPropErrArr.append(neuron.count_derivative(neuron.output) * sumIntAndArray(backPropErrTerm[i], nextLayerWeights))
                     i+=1
                 backPropErrTerm = tempBackPropErrArr
                 finalErrTermArr.extend(backPropErrTerm)
@@ -73,15 +73,26 @@ class NeuralNetwork:
             nextlayer = layer
         #save errTerm array in attribute
         self.errTerm = finalErrTermArr
-        print(finalErrTermArr)
-        return grad
+        #print(finalErrTermArr)
+        return finalErrTermArr
 
     #print all neuron inputs
     def printAllNeuronInput(self):
         for layer in self.layers:
             for neuron in layer.neurons:
                 neuron.printInput()
-
+        
+    #print neuron weights
+    def printAllNeuronWeight(self):
+        for layer in self.layers:
+            for neuron in layer.neurons:
+                print(neuron.weight)
+    
+    #print neuron weights
+    def printAllNeuronOutput(self):
+        for layer in self.layers:
+            for neuron in layer.neurons:
+                print(neuron.output)
     #update all weights by input
     #layers is reversed, because input should be a reversed array containing negative gradient
     def update_weights(self, input, learn_rate):
